f844045c7aaf0133ec1fb788e5f496ef1742fbc4
miles <pablo.riera@gmail.com>
2016-04-03 17:21:11 -0300

    doc

--- doc/conf.py
+++ doc/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pymam'
-copyright = u'2016, Author'
-author = u'Author'
+copyright = u'2016, Pablo E. Riera'
+author = u'Pablo E. Riera'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
