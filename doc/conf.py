21871e4d1319ade468dcb2b2ad96c45099c3d512
Pablo Riera <pablo.riera@gmail.com>
2017-09-14 16:48:32 -0300

    doc

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # pymam documentation build configuration file, created by
-# sphinx-quickstart on Thu Sep 14 16:47:29 2017.
+# sphinx-quickstart on Thu Sep 14 16:48:30 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
