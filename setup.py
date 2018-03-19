421fba7ff5f798239b5e6028e59f6b34c1e3c996
Pablo Riera <pablo.riera@gmail.com>
2018-03-18 22:36:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='pymam',
       version='1.0',
       packages = ['pymam'],
-      install_requires=['music21','sounddevice','pygame']
+      install_requires=['music21','sounddevice','pygame','librosa']
       )
 
 # Doc generation
